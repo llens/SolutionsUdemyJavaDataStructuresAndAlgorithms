0d4df200e43bde7a0ef92b1691d3a145018b1331
Tom Snell <tstomsnell@gmail.com>
2021-12-08 20:41:02 -0700

    Two Sum

---
+++ src/main/java/array/TwoSum.java
@@ -0,0 +1,21 @@
+package array;
+
+import java.util.ArrayList;
+
+public class TwoSum {
+
+    private TwoSum() {}
+
+    static int[] twoSum(int[] nums, int target) {
+        ArrayList<Integer> pairIndices = new ArrayList<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (target == nums[i] + nums[j]) {
+                    pairIndices.add(i);
+                    pairIndices.add(j);
+                }
+            }
+        }
+        return pairIndices.stream().mapToInt(it -> it).toArray();
+    }
+}
---
+++ src/test/java/array/TwoSumTest.java
@@ -0,0 +1,18 @@
+package array;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TwoSumTest {
+
+    @Test
+    void twoSumTest() {
+        assertArrayEquals(new int[] {0, 1}, TwoSum.twoSum(new int[] {2, 7, 11, 15}, 9));
+    }
+
+    @Test
+    void twoSumTestTwo() {
+        assertArrayEquals(new int[] {1, 2}, TwoSum.twoSum(new int[] {3, 2, 4}, 6));
+    }
+}
