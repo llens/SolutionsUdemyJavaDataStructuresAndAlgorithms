4867e20f7feb34e421139c146ae7f1afab915221
Tom Snell <tstomsnell@gmail.com>
2021-12-16 19:31:50 -0700

    Array pairs fix

--- src/main/java/time_complexity/ArrayPairs.java
+++ src/main/java/time_complexity/ArrayPairs.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicInteger;
+
 import util.Pair;
 
 public class ArrayPairs {
@@ -11,10 +14,11 @@ public class ArrayPairs {
   private ArrayPairs() {}
 
   public static List<Map.Entry<Integer, Integer>> pairsFromArray(int[] array) {
+    AtomicInteger atomicInteger = new AtomicInteger(1);
     return Arrays.stream(array)
         .mapToObj( // O(N)
             entry ->
-                Arrays.stream(array) // O(N)
+                Arrays.stream(Arrays.copyOfRange(array, atomicInteger.getAndIncrement(), array.length)) // O(N)
                     .mapToObj(entryInner -> Pair.of(entry, entryInner))
                     .toList())
         .flatMap(Collection::stream)
